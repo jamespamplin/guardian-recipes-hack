ed64795b4b142802a12caea68f32ef92eac3fbc4
James Pamplin <james.pamplin@guardian.co.uk>
2014-07-14 14:13:23 +0000

    Displaying basic recipe data

--- public/recipe-view/recipe-view.js
+++ public/recipe-view/recipe-view.js
@@ -2,6 +2,24 @@
 import angular from 'angular';
 
 angular.module('recipeView', [])
-  .controller('Recipe', [function() {
+  .controller('Recipe', ['$scope', '$routeParams', 'recipeService', function($scope, $routeParams, recipeService) {
 
+    recipeService.get($routeParams.recipeId).success(function(data) {
+      $scope.recipe = data;
+
+    }).error(function(err) {
+      console.log('error loading recipe data', $routeParams.recipeId, err);
+    });
+
+  }])
+
+  .factory('recipeService', ['$http', function($http) {
+    return {
+      get: function(recipeId) {
+        return $http({
+          method: 'GET',
+          url: '/recipe-data/' + recipeId + '.json'
+        });
+      }
+    };
   }]);
