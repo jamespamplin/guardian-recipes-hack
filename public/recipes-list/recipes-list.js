c8b816d73dc5b7616df54a3fd2facbf5177e7c7a
James Pamplin <james.pamplin@guardian.co.uk>
2014-07-15 09:01:31 +0000

    Added stubbed recipe list index page

--- public/recipes-list/recipes-list.js
+++ public/recipes-list/recipes-list.js
@@ -2,6 +2,22 @@
 import angular from 'angular';
 
 angular.module('recipesList', [])
-  .controller('RecipeList', [function() {
+  .controller('RecipesList', ['$scope', 'recipeListService', function($scope, recipeListService) {
+    recipeListService.get().success(function(data) {
+      $scope.recipesList = data;
 
+    }).error(function(err) {
+      console.log('error loading recipe list data', err);
+    });
+  }])
+
+  .factory('recipeListService', ['$http', function($http) {
+    return {
+      get: function() {
+        return $http({
+          method: 'GET',
+          url: 'recipe-data/index.json'
+        });
+      }
+    };
   }]);
