23eb958055aa1141e8ca0b7711a507378f81dc9b
James Pamplin <james.pamplin@guardian.co.uk>
2014-07-15 13:15:04 +0000

    Added timer

--- public/config.js
+++ public/config.js
@@ -10,6 +10,7 @@ System.config({
   "map": {
     "angular": "github:angular/bower-angular@1.2.18",
     "angular-route": "github:components/angular-route@^1.2.0",
+    "angular-easy-pie-chart": "github:rendro/easy-pie-chart@2.1.4/dist/angular.easypiechart.min",
     "bootstrap": "github:twbs/bootstrap@3.2.0",
     "css": "github:systemjs/plugin-css@^0.1.0",
     "jquery": "github:components/jquery@^2.1.1"
--- public/recipe-view/recipe-view.js
+++ public/recipe-view/recipe-view.js
@@ -1,8 +1,9 @@
 
 import angular from 'angular';
+import 'angular-easy-pie-chart';
 
-angular.module('recipeView', [])
-  .controller('Recipe', ['$scope', '$routeParams', 'recipeService', function($scope, $routeParams, recipeService) {
+angular.module('recipeView', ['easypiechart'])
+  .controller('Recipe', ['$scope', '$interval', '$routeParams', 'recipeService', function($scope, $interval, $routeParams, recipeService) {
 
     recipeService.get($routeParams.recipeId).success(function(data) {
       $scope.recipe = data;
@@ -11,7 +12,62 @@ angular.module('recipeView', [])
       console.log('error loading recipe data', $routeParams.recipeId, err);
     });
 
+
+    $scope.timerOptions = {
+      lineWidth: 6,
+      barColor: '#951c55'
+    };
+
+    $scope.timerValue = 100;
+    $scope.timerValueSeconds = 300;
+    $scope.timerStarted = false;
+
+    var timer,
+    timerMax = 300;
+
+    $scope.resetTimer = function() {
+      if (timer) {
+        $interval.cancel(timer);
+      }
+      $scope.timerValue = 100;
+      $scope.timerValueSeconds = timerMax;
+      $scope.timerStarted = false;
+    };
+
+    $scope.toggleTimer = function() {
+
+      if (timer) {
+        $interval.cancel(timer);
+      }
+
+      $scope.timerStarted = !$scope.timerStarted;
+
+      if ($scope.timerStarted) {
+        // $scope.timerValue = 100;
+        // $scope.timerValueSeconds = timerMax;
+
+        timer = $interval(function() {
+          $scope.timerValueSeconds--;
+          $scope.timerValue = $scope.timerValueSeconds / timerMax * 100;
+
+          if (!$scope.timerValue) {
+            $interval.cancel(timer);
+            $scope.timerStarted = false;
+          }
+        }, 1000);
+      }
+    };
+
   }])
+
+  .filter('formatTimer', function() {
+    return function(input) {
+      var minutes = Math.floor(input / 60),
+      seconds = input - minutes * 60;
+
+      return minutes + 'min ' + seconds + 's';
+    };
+  })
 
   .directive('recipeTechnique', [function() {
 
